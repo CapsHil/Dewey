28bda4d70e6e0b967bf5bfbd4912d612549cbb20
Pierre RABY <Pierre@mbp-de-pierre.home>
2014-08-08 13:21:10 +0200

    Initial Commit

---
+++ Training Lab Game/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Training Lab Game
+//
+//  Created by Pierre RABY on 08/08/2014.
+//  Copyright (c) 2014 Pierre RABY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
